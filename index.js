354037b1195e9a90df36566805f8e0478d44d6bf
mdyasin111 <mdyasin7636@gmail.com>
2023-06-09 18:48:12 +0600

    admin role set

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000;
 
 // middleware
@@ -47,6 +47,19 @@ async function run() {
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    })
+
+    app.patch('users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
     })
 
 
