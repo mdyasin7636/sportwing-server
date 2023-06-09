cd0fd98b0dbef26b5c461ed2b2a27530330587ce
mdyasin111 <mdyasin7636@gmail.com>
2023-06-09 17:31:56 +0600

    getting all user

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
 
 
     // users related apis
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
     
     app.post('/users', async(req, res) => {
       const user = req.body;
