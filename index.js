8027c1e2e814490e87b06767efe31fea7c1cb328
mdyasin111 <mdyasin7636@gmail.com>
2023-06-09 10:51:07 +0600

    user api done

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
+
+    const usersCollection = client.db('sportWingDb').collection('users');
+
+
+    // users related apis
+    
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
