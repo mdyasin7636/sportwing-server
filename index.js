b918abfe8ddf52cdb748c6248a925ee272fe85dd
mdyasin111 <mdyasin7636@gmail.com>
2023-11-01 15:39:52 +0600

    upadted

--- index.js
+++ index.js
@@ -51,8 +51,11 @@ async function run() {
     const paymentCollection = client.db('sportWingDb').collection('payments');
 
 
+
+
     app.post('/jwt', (req, res) => {
       const user = req.body;
+      
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5h'});
       res.send({ token });
     })
